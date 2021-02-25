9e2770fefdbae426f5aac4912907237ca74e230a
Hoai Thai <khanhhoai95@gmail.com>
2021-02-25 16:55:21 +0700

    fix: unavailable image issue

--- src/constants/moviePoster.ts
+++ src/constants/moviePoster.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_POSTER =
-  "https://i1.wp.com/www.sacredheartschoolweymouth.org/wp-content/uploads/revslider/homeslider_boxed/woman1.png?fit=260%2C600&ssl=1";
+  "https://images-na.ssl-images-amazon.com/images/I/11Pl7si+xBL._SX331_BO1,204,203,200_.jpg";
 export const MOVIE_POSTER_URL = "https://image.tmdb.org/t/p/w200";
 export const MOVIE_POSTER_URL_W200 = "https://image.tmdb.org/t/p/w200";
