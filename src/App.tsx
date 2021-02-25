7d2a7894f8121b762e37039ef0dcbb018820582b
Hoai Thai <khanhhoai95@gmail.com>
2021-02-25 11:40:39 +0700

    init: ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import Header from "./Header";
 import SearchPage from "src/pages/SearchPage/SearchPage";
 import HomePage from "src/pages/HomePage/HomePage";
+import ThemeContext from "./context/ThemeContext";
 
 const App = () => {
+  const [theme, setTheme] = useState("dark");
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+        <div className={`App ${theme}`}>
+          <Header />
+          <Switch>
+            <Route path="/search">
+              <SearchPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </ThemeContext.Provider>
     </BrowserRouter>
   );
 };
